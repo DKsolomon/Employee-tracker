2edb2e243d1d03432df226896b4a204f637af5e0
DKsolomon <DKSolomon8135@gmail.com>
2023-05-22 22:51:00 -0700

    added funtions for first 3 options, pushing for debugging with inqirer

--- server.js
+++ server.js
@@ -15,57 +15,82 @@ function init(){
   inquirer
       .prompt({
           
-              type: 'list',
-              message: 'What would you like to do?',
-              name: 'option',
-              choices: [
-                  'View all departments',
-                  'View all roles',
-                  'View all employees',
-                  'Add A department',
-                  'Add A role',
-                  'Add an employee',
-                  'Update an employee role',
-                  'Quit'
-              ]
-            })
-            .then(function(answer) {
+          type: 'list',
+          message: 'What would you like to do?',
+          name: 'options',
+          choices: [
+              'View all departments',
+              'View all roles',
+              'View all employees',
+              'Add A department',
+              'Add A role',
+              'Add an employee',
+              'Update an employee role',
+              'Quit'
+          ]
+        })
+        .then(function(answer) {
 
-              switch(answer.action) {
-                  case 'View all departments':
-                      viewDepartments();
-                      break;
+          switch(answer.action) {
+              case 'View all departments':
+                  viewDepartments();
+                  break;
                   
-                  case 'View all roles':
-                      viewRoles();
-                      break;
+              case 'View all roles':
+                  viewRoles();
+                  break;
       
-                  case 'View all employees':
-                      viewEmployees();
-                      break;
+              case 'View all employees':
+                  viewEmployees();
+                  break;
       
-                  case 'Add A department':
-                      addDepartment();
-                      break;
+              case 'Add A department':
+                  addDepartment();
+                  break;
       
-                  case 'Add a role':
-                      addRole();
-                      break;
+              case 'Add a role':
+                   addRole();
+                   break;
       
-                  case 'Add an employee':
-                      addEmployee();
-                      break;
+              case 'Add an employee':
+                  addEmployee();
+                  break;
       
-                  case 'Update an employee role':
-                      updateEmployeeRole();
-                      break;
+              case 'Update an employee role':
+                  updateEmployeeRole();
+                  break;
       
-                  case 'Quit':
-                      connection.end();
-                      break;
-              }
-      
+              case 'Quit':
+                  connection.end();
+                  break;
+          }
             })
-      };
+  };
         
+function viewDepartments() {
+  let query = "SELECT * FROM departments";
+  db.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    init();
+  });
+}
+
+function viewRoles() {
+  let query = "SELECT * FROM roles";
+  db.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    init()
+  });
+}
+
+function viewEmployees() {
+  let query = "SELECT * FROM employees";
+  db.query(query, function (err, res) {
+    if (err) throw err;
+    console.table(res)
+    init()
+  });
+}
 init();
